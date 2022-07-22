6cb115714991ffd6def82ebdd3f0e719a837dab0
Ko en Cheong <kcheong@u82z04s03.42kl.edu.my>
2022-07-22 21:28:35 +0800

    Yay

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kcheong <kcheong@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 14:51:04 by kcheong           #+#    #+#             */
-/*   Updated: 2022/07/13 14:51:04 by kcheong          ###   ########.fr       */
+/*   Updated: 2022/07/22 16:24:10 by kcheong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,16 @@ char	*store_extra(char *storage)
 	char	*extra;
 
 	i = 0;
-	j = 0;
-	extra = NULL;
 	while (storage[i] != '\0' && storage[i] != '\n')
 		i++;
-	if (storage[i] == '\n')
-	{
-		i++;
-		extra = ft_calloc(ft_strlen(storage) - i + 1, sizeof(char));
-		while (storage[i] != '\0')
-			extra[j++] = storage[i++];
-	}
+	extra = ft_calloc((ft_strlen(storage) - i + 1), sizeof(char));
+	i++;
+	j = 0;
+	// printf("Storage that is being brought into store_extra = %s\n", storage);
+	while (storage[i] != '\0')
+		extra[j++] = storage[i++];
+	free(storage);
+	// printf("Extra = %s\n", extra);
 	return (extra);
 }
 
@@ -42,6 +41,7 @@ char	*return_line(char *storage)
 	while (storage[i] != '\0' && storage[i] != '\n')
 		i++;
 	line = ft_calloc(i + 2, sizeof(char));
+	i = 0;
 	while (storage[i] != '\0' && storage[i] != '\n')
 	{
 		line[i] = storage[i];
@@ -52,20 +52,30 @@ char	*return_line(char *storage)
 	return (line);
 }
 
-char	*read_file(int fd, char *buffer)
+char	*read_file(int fd, char *storage)
 {
 	int		byteread;
-	char	*storage;
+	char	*buffer;
 
 	byteread = 1;
+	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	while (byteread > 0)
 	{
 		byteread = read(fd, buffer, BUFFER_SIZE);
-		buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-		buffer[byteread] = '\0';
-		storage = ft_strjoin(storage, buffer);
-		if (ft_strchr(storage, '\n'))
-			break ;
+		// printf("[%s]\n", buffer);
+		buffer[byteread] = 0;
+		if (!storage)
+		{
+			storage = ft_strdup(buffer);
+			// printf("Initial storage = [%s]\n", storage);
+		}
+		else
+		{
+			storage = ft_strjoin(storage, buffer);
+			// printf("Read until new line is found = [%s]\n", storage);
+			if (ft_strchr(storage, '\n'))
+				break ;
+		}
 	}
 	free (buffer);
 	return (storage);
@@ -76,17 +86,54 @@ char	*get_next_line(int fd)
 	static char	*storage;
 	char		*line;
 
-	if (!storage)
-		storage = ft_calloc(1, 1);
 	storage = read_file(fd, storage);
+	// printf("Storage while reading the file = %s\n", storage);
 	line = return_line(storage);
 	storage = store_extra(storage);
+	// printf("Line being returned = %s\n", line);
+	// printf("Storage after storing extra characters = %s\n", storage);
 	return (line);
 }
 
-int	main(void)
+char	*ft_strdup(const char *s1)
 {
-	int	fd = open("test.txt", O_RDONLY);
-	printf("%s", get_next_line(fd));
-	// get_next_line(fd);
+	char	*temp;
+	size_t	i;
+	size_t	s1len;
+
+	i = 0;
+	s1len = ft_strlen(s1);
+	temp = malloc(sizeof(char) * (s1len + 1));
+	if (!temp)
+		return (NULL);
+	while (s1[i])
+	{
+		temp[i] = s1[i];
+		i++;
+	}
+	temp[i] = '\0';
+	return (temp);
 }
+
+// int	main(void)
+// {
+// 	char *res;
+	
+// 	int	fd = open("test.txt", O_RDONLY);
+// 	res = get_next_line(fd);
+// 	printf("Result 1 = [%s]\n", res);
+// 	free(res);
+// 	res = get_next_line(fd);
+// 	printf("Result 2 = [%s]\n", res);
+// 	free(res);
+// 	res = get_next_line(fd);
+// 	printf("Result 3 = [%s]\n", res);
+// 	free(res);
+// 	res = get_next_line(fd);
+// 	printf("Result 4 = [%s]\n", res);
+// 	free(res);
+// 	// printf("%s", get_next_line(fd));
+// 	// get_next_line(fd);
+// 	// get_next_line(fd);
+// 	// get_next_line(fd);
+// }
--- get_next_line.h
+++ get_next_line.h
@@ -26,6 +26,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
 void	*ft_calloc(size_t count, size_t size);
 void	ft_bzero(void *s, size_t n);
+char	*ft_strdup(const char *s1);
 
 #endif
 
